66554346622d52bccb88b62bdb76bc5fccb0b43c
karissa <krmckelv@gmail.com>
2018-03-26 17:33:58 +0200

    add dummy storage

--- content/index.js
+++ content/index.js
@@ -1,3 +1,21 @@
-autocrypt = require('autocrypt')
+var autocrypt = require('autocrypt')
 
-EXPORTED_SYMBOLS = [ 'autocrypt' ]
+var myStorage = {
+  get: function (key, cb) { return cb() },
+  put: function (key, data, cb) { return cb() },
+  del: function (key, cb) { return cb()}
+}
+var ac = autocrypt({
+  storage: myStorage
+})
+
+EXPORTED_SYMBOLS = [ 'Autocrypt' ]
+
+Autocrypt = {
+  addUser: function (email, publicKey, opts, cb) {
+    ac.addUser(email, publicKey, opts, cb)
+  },
+  getUser: function (email, cb) {
+    ac.getUser(email, cb)
+  }
+}
--- content/messenger.js
+++ content/messenger.js
@@ -7,32 +7,34 @@ window.addEventListener("load", function(e) {
   startup();
 }, false);
 
+function done (err, user) {
+  if (err) throw err
+  var myPanel = document.getElementById("autocrypt-panel");
+  myPanel.label = "Autocrypt: Enabled"
+  console.log('done', user)
+}
+
 function startup() {
   var email = 'jonnyappleseed@gmail.com'
   console.log ('hi', email)
-
   Autocrypt.getUser(email, function (err, user) {
     if (err) {
       console.error(err)
       return generateKey(email, done)
     }
+    console.log('got user', user)
     done(null, user)
   })
-
-  var done = function (err, user) {
-    if (err) throw err
-    var myPanel = document.getElementById("autocrypt-panel");
-    myPanel.label = "Autocrypt: Enabled"
-    console.log('user', user)
-  }
 }
 
 function generateKey (email, cb) {
+  console.log('generating key', email)
   openpgp.generateKey({
     userIds: [{ name: 'Jon Smith', email: email }],
     numBits: 3072,
     passphrase: 'super long and hard to guess'
   }).then((key) =>  {
+    console.log('adding user')
     Autocrypt.addUser(email, key.publicKeyArmored, function (err) {
       if (err) return cb(err)
       console.log('created new user')
